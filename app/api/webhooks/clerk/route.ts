7040302a61134097cd88e54d1461084339f124ca
Alejo Berenguer <alejoberenguer5@gmail.com>
2026-05-18 21:54:49 -0300

    fix bugs

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -26,7 +26,7 @@ export async function POST(req: Request) {
     );
   }
 
-  const headerList = headers();
+  const headerList = await headers();
   const svixId = headerList.get('svix-id');
   const svixTimestamp = headerList.get('svix-timestamp');
   const svixSignature = headerList.get('svix-signature');
